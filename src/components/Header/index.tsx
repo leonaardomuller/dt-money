d4f714de02189e033448e9e700b6d7408eb4e4e5
leonaardomuller <60147880+leonaardomuller@users.noreply.github.com>
2022-09-09 18:19:43 -0300

    including Radix-ui

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,8 @@ import {
   HeaderContent,
   NewTransactionButton,
 } from '../Header/styles';
+
+import * as Dialog from '@radix-ui/react-dialog';
 import logoImg from '../../assets/logo.svg';
 
 export function Header() {
@@ -10,7 +12,20 @@ export function Header() {
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <NewTransactionButton>Nova transacao</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay>
+              <Dialog.Content>
+                <Dialog.Title>Nova transação</Dialog.Title>
+                <Dialog.Close>Fechar</Dialog.Close>
+              </Dialog.Content>
+            </Dialog.Overlay>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
