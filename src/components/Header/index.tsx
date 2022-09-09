4f01c7c132bdc5956b69dc81bc09e56c033c5546
leonaardomuller <60147880+leonaardomuller@users.noreply.github.com>
2022-09-09 15:54:51 -0300

    Initial commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import {
+  HeaderContainer,
+  HeaderContent,
+  NewTransactionButton,
+} from '../Header/styles';
+import logoImg from '../../assets/logo.svg';
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
+        <NewTransactionButton>Nova transacao</NewTransactionButton>
+      </HeaderContent>
+    </HeaderContainer>
+  );
+}
