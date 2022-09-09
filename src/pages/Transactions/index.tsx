1609f9beec456b3f02d61d9aaa1a44f50895116e
leonaardomuller <60147880+leonaardomuller@users.noreply.github.com>
2022-09-09 17:16:39 -0300

    SearchForm

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,6 +1,8 @@
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
+import { SearchForm } from '../Transactions/components/SearchForm';
 import {
+  PriceHighlight,
   TransactionsContainer,
   TransactionsTable,
 } from '../Transactions/styles';
@@ -12,17 +14,23 @@ export function Transactions() {
       <Summary />
 
       <TransactionsContainer>
+        <SearchForm />
+
         <TransactionsTable>
           <tbody>
             <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td>12.000,00</td>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income"> R$ 12.000,00</PriceHighlight>
+              </td>
               <td>Venda</td>
               <td>13/04/2022</td>
             </tr>
             <tr>
-              <td width="50%">Hamburger</td>
-              <td>- 59,00</td>
+              <td width="40%">Hamburger</td>
+              <td>
+                <PriceHighlight variant="outcome">R$ - 59,00</PriceHighlight>
+              </td>
               <td>Alimentacao</td>
               <td>10/04/2022</td>
             </tr>
