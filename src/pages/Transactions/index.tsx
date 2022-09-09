cfef80ec5d6fb1dfde78593994068ac97924073d
leonaardomuller <60147880+leonaardomuller@users.noreply.github.com>
2022-09-09 16:28:00 -0300

    2 files modified

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,34 @@
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
+import {
+  TransactionsContainer,
+  TransactionsTable,
+} from '../Transactions/styles';
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>12.000,00</td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>- 59,00</td>
+              <td>Alimentacao</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
