c7e9a027b0699497749c2f1fc23f813edbef3251
leonaardomuller <60147880+leonaardomuller@users.noreply.github.com>
2022-09-11 02:33:36 -0300

    Finalizada Estrutura Visual

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import {
   HeaderContent,
   NewTransactionButton,
 } from '../Header/styles';
+import { NewTransactionModal } from '../NewTransactionModal';
 
 import * as Dialog from '@radix-ui/react-dialog';
 import logoImg from '../../assets/logo.svg';
@@ -17,14 +18,7 @@ export function Header() {
             <NewTransactionButton>Nova transação</NewTransactionButton>
           </Dialog.Trigger>
 
-          <Dialog.Portal>
-            <Dialog.Overlay>
-              <Dialog.Content>
-                <Dialog.Title>Nova transação</Dialog.Title>
-                <Dialog.Close>Fechar</Dialog.Close>
-              </Dialog.Content>
-            </Dialog.Overlay>
-          </Dialog.Portal>
+          <NewTransactionModal />
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
